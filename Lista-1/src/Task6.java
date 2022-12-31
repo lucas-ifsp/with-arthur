71bb036afdfbb64aff47ef3b4197a6f85d5de6ff
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 14:49:33 -0300

    refactor (Ex6): fix typos in variable names

--- Lista-1/src/Task6.java
+++ Lista-1/src/Task6.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] firstVetor = new int[5];
-        int[] secondVetor = new int[5];
-        int[] thirdVetor = new int[5];
+        int[] vectorA = new int[5];
+        int[] vectorB = new int[5];
+        int[] vectorC = new int[5];
+
         for (int i = 0; i < 5; i++) {
-            System.out.println("Type a number for the first Vetor: ");
-            firstVetor[i] = scanner.nextInt();
+            System.out.println("Type a number for the first vector: ");
+            vectorA[i] = scanner.nextInt();
         }
         for (int i = 0; i < 5; i++) {
-            System.out.println("Type a number for the second Vetor: ");
-            secondVetor[i] = scanner.nextInt();
+            System.out.println("Type a number for the second vector: ");
+            vectorB[i] = scanner.nextInt();
         }
         scanner.close();
+
         for (int i = 0; i < 5; i++) {
-            thirdVetor[i] = firstVetor[i] + secondVetor[i];
+            vectorC[i] = vectorA[i] + vectorB[i]; // This code is prone to over and under flow. Can you notice?
+            // We will fix it in the next commit!
         }
-            System.out.println(Arrays.toString(thirdVetor));
+        System.out.println(Arrays.toString(vectorC));
     }
 }
