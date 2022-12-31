44757e2e318eab2b34654b9b60665efad951ebe8
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 12:59:58 -0300

    docs (Ex1): remove unnecessary comments

--- Lista-1/src/Task1.java
+++ Lista-1/src/Task1.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        //Variables
+        // >> only use comments for things that cannot be inferred directly by reading a good code. <<
+
         Scanner scanner = new Scanner(System.in);
         // notice that we don't need a global counting variable (we can simply use 'for' scoped variable "i")
         //int totalNumbers = 0;
@@ -11,7 +12,6 @@ public class Task1 {
 
         int[] integers = new int[5];
 
-        //prompt
 /*        do {
             System.out.println("Write a Integer Number:");
             int typedInteger = scanner.nextInt();
@@ -29,12 +29,10 @@ public class Task1 {
         int biggestNumber = integers[0]; // the first is always the biggest when only the first is known
         int smallerNumber = integers[0]; // same idea
 
-        //Finding numbers
         for (int number : integers) {
             if (number > biggestNumber) biggestNumber = number;
             if (number < smallerNumber) smallerNumber = number;
         }
-        //Printing results
         System.out.println("Biggest number: " + biggestNumber);
         System.out.println("Smallest number: " + smallerNumber);
     }
