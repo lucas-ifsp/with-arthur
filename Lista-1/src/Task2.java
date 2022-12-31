cc44332472f7801799b161cab1a06cb660d959c9
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 13:41:01 -0300

    refactor (Ex2): extract recursive factorial as a method and remove unnecessary inner class

--- Lista-1/src/Task2.java
+++ Lista-1/src/Task2.java
@@ -3,23 +3,14 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
 
-        //Recursive method
-        class Factorial {
-            public static int factorial(int number) {
-                if (number == 1) {
-                    return 1;
-                }
-                if (number == 0) {
-                    return 0;
-                }
-                return number * factorial(number - 1);
-            }
-        }
+        //avoid using inner classes (it is not a good practice in most cases)
+        //class Factorial {
+        //}
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write a Number:");
         int factorialNumber = scanner.nextInt();
-        int resultFactorialMethod = Factorial.factorial(factorialNumber);
+        int resultFactorialMethod = recursiveFactorial(factorialNumber);
         System.out.println("Result of Factorial (recursive method): " + resultFactorialMethod);
 
 
@@ -33,6 +24,11 @@ public class Task2 {
              result *= i;
         }
         return result;
+    }
+
+    public static int recursiveFactorial(int number) {
+        if (number == 0 || number == 1) return 1;
+        return number * recursiveFactorial(number - 1);
     }
 }
 
