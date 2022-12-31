c8a7047018b908d4b24725c34217d1bac42b5a31
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 16:23:46 -0300

    refactor (Ex9): simplify a lot chocolate amount calculation

--- Lista-1/src/Task9.java
+++ Lista-1/src/Task9.java
@@ -10,24 +10,32 @@ public class Task9 {
         System.out.print("Digite o preço do chocolate: ");
         double price = scanner.nextDouble();
 
-        System.out.print("Digite embalagens para um chocolate grátis?: ");
-        int freeGift = scanner.nextInt();
+        System.out.print("Digite embalagens para um chocolate grátis: ");
+        int wrappersPerChocolate = scanner.nextInt();
 
-        if(freeGift <= 1) {
+        if(wrappersPerChocolate <= 1) {
             System.out.println("Erro! O número de embalagens deve ser maior que um.");
-            return; //returns from main. In other words, ends the program.
+            return;
         }
 
-        if (price > money){
+        //This solution is simples, but still have a bug. Can you find it?
+        //How many free chocolates would Xoze get if he purchases 100 chocolates and the
+        //promotion gives him a free chocolate of every 10 wrappers?
+        int paidChocolates = (int) (money / price);
+        int freeChocolates = paidChocolates / wrappersPerChocolate;
+
+        System.out.printf("Xozé comprou %d chocolates e ganhou %d chocolates de graça.\n", paidChocolates, freeChocolates);
+
+        /*if (price > money){
             System.out.println("Xozé ta duro e n pode comprar nenhum chocolate");
         } else if (price == money) {
             System.out.println("Parabens, Xozé conseguiu comprar um chocolate");
         } else {
             int quantity = (int) Math.round(money / price);
-            if (quantity >= freeGift){
+            if (quantity >= wrappersPerChocolate){
                 System.out.println("Xozé pode comprar: " + quantity + " Chocolates, e se devolver as embalagens " +
-                        "conseguirá mais: " + Math.round(quantity/freeGift));
+                        "conseguirá mais: " + Math.round(quantity/wrappersPerChocolate));
             }
-        }
+        }*/
     }
 }
