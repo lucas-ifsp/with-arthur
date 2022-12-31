b75e8798714aad30b60a3fc5e91974ef78d3c9e4
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 14:55:01 -0300

    perf (Ex6): improve performance by removing unnecessary summing loop

--- Lista-1/src/Task6.java
+++ Lista-1/src/Task6.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] vectorA = new int[5];
-        int[] vectorB = new int[5];
-        long[] vectorC = new long[5];
+        final var scanner = new Scanner(System.in);
+        final var vectorA = new int[5];
+        final var vectorB = new int[5];
+        final var vectorC = new long[5];
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Type a number for the first vector: ");
@@ -15,15 +15,9 @@ public class Task6 {
         for (int i = 0; i < 5; i++) {
             System.out.println("Type a number for the second vector: ");
             vectorB[i] = scanner.nextInt();
-        }
-        scanner.close();
-
-        for (int i = 0; i < 5; i++) {
-            //if we sum two very big ints, the result can be bigger than an int variable can hold.
-            //solution 1: use long to store the result of summing two ints
-            //solution 2: check if the sum is bigger and, in this case, throw a runtime exception.
             vectorC[i] = vectorA[i] + vectorB[i];
         }
+        scanner.close();
         System.out.println(Arrays.toString(vectorC));
     }
 }
