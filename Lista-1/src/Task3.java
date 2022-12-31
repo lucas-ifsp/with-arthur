8f02ea0ee3a52eea40b83c18efa0825ddf51d693
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 13:54:11 -0300

    refactor (Ex3): organize code that reads the input

--- Lista-1/src/Task3.java
+++ Lista-1/src/Task3.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Type a Number: ");
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type a number: ");
+
+        final var scanner = new Scanner(System.in); //yes, Java has type inference for local variables
         int number = scanner.nextInt();
+        scanner.close();
+
         while (number != 1) {
             if (number % 2 == 0){
-                number = number/2;
+                number = number / 2;
                 System.out.println(number);
             } else {
-                number = number*3 +1;
+                number = number * 3 + 1;
                 System.out.println(number);
             }
         }
