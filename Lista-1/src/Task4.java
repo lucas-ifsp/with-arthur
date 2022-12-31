f50ee19e5b35e36c40324b9943302d3c76ac2b54
lucas-ifsp <lucas.ruas@gmail.com>
2022-12-31 14:07:45 -0300

    refactor (Ex4): extract methods out of inner class and then remove the class

--- Lista-1/src/Task4.java
+++ Lista-1/src/Task4.java
@@ -2,48 +2,46 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
-
-        class Fibonacci {
-
-            //Recursive method
-            public static int fibonacci(int number) {
-                if (number == 0 || number == 1)
-                    return number;
-                return fibonacci(number - 1) + fibonacci(number - 2);
-            }
-
-            //Iterative method
-            public static int fibonacciInterative(int number) {
-                int fibonacci = 0;
-                int previousFibonacci = 0;
-                for (int i = 1; i <= number; i++) {
-                    if (i == 1) {
-                        fibonacci = 1;
-                        previousFibonacci = 0;
-                    } else {
-                        fibonacci += previousFibonacci;
-                        previousFibonacci = fibonacci - previousFibonacci;
-                    }
-                }
-                return fibonacci;
-            }
-
-        }
         System.out.println("Type a number: ");
-        Scanner scanner = new Scanner(System.in);
-        int fibonacciNumber = scanner.nextInt();
+
+        final var scanner = new Scanner(System.in);
+        final int fibonacciNumber = scanner.nextInt();
         scanner.close();
 
         //Recursive method
         for (int i = 0; i < fibonacciNumber; i++) {
-            System.out.println("Result (Recursive): " + Fibonacci.fibonacci(i));
+            System.out.println("Result (Recursive): " + recursiveFibonacci(i));
         }
 
         System.out.println();
 
         //Iterative method
         for (int j = 0; j < fibonacciNumber; j++) {
-            System.out.println("Result (Iterative): " + Fibonacci.fibonacciInterative(j));
+            System.out.println("Result (Iterative): " + iterativeFibonacci(j));
         }
+    }
+
+
+    //Recursive method
+    public static int recursiveFibonacci(int number) {
+        if (number == 0 || number == 1) return number;
+        return recursiveFibonacci(number - 1) + recursiveFibonacci(number - 2);
+    }
+
+    //Iterative method
+    public static int iterativeFibonacci(int number) {
+        int fibonacci = 0;
+        int previousFibonacci = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (i == 1) {
+                fibonacci = 1;
+                previousFibonacci = 0;
+            } else {
+                fibonacci += previousFibonacci;
+                previousFibonacci = fibonacci - previousFibonacci;
+            }
+        }
+        return fibonacci;
     }
 }
