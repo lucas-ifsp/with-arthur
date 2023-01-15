f71c494f8500612771a5d0593935f8ee6d1e9fdb
lucas-ifsp <lucas.ruas@gmail.com>
2023-01-14 22:44:09 -0300

    fix: remove unnecessary this operators, as there is no shadowing

--- src/main/java/br/ifsp/ic/petshop/Petshop.java
+++ src/main/java/br/ifsp/ic/petshop/Petshop.java
@@ -9,25 +9,25 @@ public class Petshop {
 
     public Petshop() {
     }
-    
+
     public void addCliente(Pessoa cliente){
         this.clientes.putIfAbsent(cliente.getCpf(), cliente);
     }
 
     public void addCachorro(String cpf, Cachorro cachorro){
-        if (this.clientes.get(cpf) == null)
+        if (clientes.get(cpf) == null)
             throw new IllegalArgumentException("Cpf passado não possuí cliente cadastrado no sistema");
-        this.clientes.get(cpf).addCachorro(cachorro);
+        clientes.get(cpf).addCachorro(cachorro);
     }
 
     public void listarClientes(){
-        for (String cpf : this.clientes.keySet()){
-            System.out.println(this.clientes.get(cpf).toString() + " \n");
+        for (String cpf : clientes.keySet()){
+            System.out.println(clientes.get(cpf).toString() + " \n");
         }
     }
 
     public void listarCachorros(String cpf){
-        if (this.clientes.get(cpf) == null)
+        if (clientes.get(cpf) == null)
             throw new IllegalArgumentException("Cpf passado não possuí cliente cadastrado no sistema");
         for (Cachorro cachorro : clientes.get(cpf).getCachorros()){
             System.out.println(cachorro.toString() + " \n");
@@ -35,23 +35,23 @@ public class Petshop {
     }
 
     public void removeCliente(String cpf){
-        if (this.clientes.get(cpf) == null)
+        if (clientes.get(cpf) == null)
             throw new IllegalArgumentException("Cpf passado não possuí cliente cadastrado no sistema");
-        this.clientes.remove(cpf);
+        clientes.remove(cpf);
     }
 
     public void removerCachorro(String cpf, String nomeCachorro){
-        if (this.clientes.get(cpf) == null)
+        if (clientes.get(cpf) == null)
             throw new IllegalArgumentException("Cpf passado não possuí cliente cadastrado no sistema");
-        Pessoa pessoa = this.clientes.get(cpf);
+        Pessoa pessoa = clientes.get(cpf);
         if (!pessoa.getCachorros().contains(nomeCachorro))
             throw new IllegalArgumentException("Nome do Cachorro passado não está em posse do cliente.");
         pessoa.removerCachorro(nomeCachorro);
     }
 
     public void listarTodosVacinados(){
-        for(String cpf : this.clientes.keySet()){
-            for (Cachorro c : this.clientes.get(cpf).getCachorros()){
+        for(String cpf : clientes.keySet()){
+            for (Cachorro c : clientes.get(cpf).getCachorros()){
                 if (c.isVacinado()){
                     System.out.println(c + " \n");
                 }
@@ -60,9 +60,9 @@ public class Petshop {
     }
 
     public void listarVacinadosPorCliente(String cpf){
-        if (this.clientes.get(cpf) == null)
+        if (clientes.get(cpf) == null)
             throw new IllegalArgumentException("Cpf passado não possuí cliente cadastrado no sistema");
-        for (Cachorro c : this.clientes.get(cpf).getCachorros()){
+        for (Cachorro c : clientes.get(cpf).getCachorros()){
             if (c.isVacinado()){
                 System.out.println(c + " \n");
             }
