919a6c2a4d41fbd5e336a863790e53ba67f9d292
lucas-ifsp <lucas.ruas@gmail.com>
2023-01-14 22:13:25 -0300

    refactor: inline figure objects into add method as they are not used elsewhere. Use polimorfism to toString figures and calculate areas

--- src/main/java/br/ifsp/ic/figures/Main.java
+++ src/main/java/br/ifsp/ic/figures/Main.java
@@ -1,21 +1,27 @@
 package br.ifsp.ic.figures;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Figure> figures = new ArrayList<>();
+        //It is better to declare variables with interface types,
+        //so you can change which implementation you want to use.
+        //List<Figure> will work no matter it is an ArrayList or
+        //an LinkedList.
+        List<Figure> figures = new ArrayList<>();
+
         for (int i = 1; i <= 50; i++) {
-            Figure circle = new Circle(i, i, i);
-            figures.add(circle);
-            Figure rectangle = new Rectangle(i, i, i, i);
-            figures.add(rectangle);
-            Figure triangle = new Triangle(i, i, i, i, i);
-            figures.add(triangle);
+            figures.add(new Circle(i, i, i));
+            figures.add(new Rectangle(i, i, i, i));
+            figures.add(new Triangle(i, i, i, i, i));
         }
 
+        double totalArea = 0;
         for (Figure figure : figures) {
-            System.out.println(figure.area());
+            totalArea += figure.area();
+            System.out.println(figure);
         }
+        System.out.println("Sum of figure areas: " + totalArea);
     }
 }
