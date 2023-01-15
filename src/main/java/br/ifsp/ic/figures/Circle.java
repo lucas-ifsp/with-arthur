93af25c712574d7c2992ffbf404affb288d9014e
lucas-ifsp <lucas.ruas@gmail.com>
2023-01-14 21:51:18 -0300

    feat: provide toString to Circle

--- src/main/java/br/ifsp/ic/figures/Circle.java
+++ src/main/java/br/ifsp/ic/figures/Circle.java
@@ -2,16 +2,20 @@ package br.ifsp.ic.figures;
 
 public class Circle extends Figure {
 
-    private double raio;
+    private final double radius;
 
-    public Circle(int x, int y, double raio) {
+    public Circle(int x, int y, double radius) {
         super(x, y);
-        this.raio = raio;
+        this.radius = radius;
     }
 
     @Override
     public double area(){
-        return Math.PI * (2 * raio);
+        return Math.PI * (2 * radius);
     }
 
+    @Override
+    public String toString() {
+        return String.format("Circle (%d, %d) with radius %.2f. Area = %.2f", getX(), getY(), radius, area());
+    }
 }
