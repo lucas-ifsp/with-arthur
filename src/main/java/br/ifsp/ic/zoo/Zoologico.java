7ee995a12e149b25a342b4ff6e3939f95325e2eb
lucas-ifsp <lucas.ruas@gmail.com>
2023-01-14 22:29:49 -0300

    refactor: introduce 'pattern matching for instanceof' feature example

--- src/main/java/br/ifsp/ic/zoo/Zoologico.java
+++ src/main/java/br/ifsp/ic/zoo/Zoologico.java
@@ -15,13 +15,14 @@ public class Zoologico {
         jaulas[8] = new Leao();
         jaulas[9] = new Coruja();
 
+        //It is correct. You nailed the concept of type checking and polymorphism.
+        //I refactored just to show you another way to do it using pattern matching for instanceof (Java 17 feature)
+        //https://docs.oracle.com/en/java/javase/17/language/pattern-matching-instanceof-operator.html
         for (Animal animal : jaulas){
             animal.emitirSom();
-            if (animal instanceof Leao) ((Leao) animal).correr();
-            if (animal instanceof Lobo) ((Lobo) animal).correr();
-        }
-
-
+            if (animal instanceof Leao leao) leao.correr();
+            if (animal instanceof Lobo lobo) lobo.correr();
         }
     }
+}
 
